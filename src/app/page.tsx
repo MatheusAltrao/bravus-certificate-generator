6fe28c7bec2ea658662b254a30ca79f8e76648d3
Matheus Altrão <matheusaltrao2@gmail.com>
2024-07-05 13:46:05 -0400

    fix: client-side exception has occurred

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,31 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
-import { BeltsProps, jiuJitsuBelts } from '@/utils/beltsname';
+
 import html2canvas from 'html2canvas';
 import { jsPDF } from 'jspdf';
 import Image from 'next/image';
 import { FormEvent, useRef, useState } from 'react';
 import Certificado from '../assets/certificado.svg';
+
+const jiuJitsuBelts = [
+  { name: 'Faixa Cinza e Branca', color1: '#949599', color2: '#000', alignment: 'text-center' },
+  { name: 'Faixa Cinza', color1: '#949599', color2: '#000', alignment: 'pr-[12%] text-right' },
+  { name: 'Faixa Cinza e Preta', color1: '#949599', color2: '#000', alignment: 'text-center' },
+  { name: 'Faixa Amarela e Branca', color1: '#E5BE01', color2: '#000', alignment: 'text-center' },
+  { name: 'Faixa Amarela', color1: '#E5BE01', color2: '#000', alignment: 'pr-[7%] text-right' },
+  { name: 'Faixa Amarela e Preta', color1: '#E5BE01', color2: '#000', alignment: 'text-center' },
+  { name: 'Faixa Laranja e Branca', color1: '#F05D28', color2: '#000', alignment: 'text-center' },
+  { name: 'Faixa Laranja', color1: '#F05D28', color2: '#000', alignment: 'pr-[8%] text-right' },
+  { name: 'Faixa Laranja e Preta', color1: '#F05D28', color2: '#000', alignment: 'text-center' },
+  { name: 'Faixa Verde e Branca', color1: '#026738', color2: '#000', alignment: 'text-center' },
+  { name: 'Faixa Verde', color1: '#026738', color2: '#000', alignment: 'pr-[12%] text-right' },
+  { name: 'Faixa Verde e Preta', color1: '#026738', color2: '#000', alignment: 'text-center' },
+  { name: 'Faixa Azul', color1: '#1B74BD', color2: '#000', alignment: 'pr-[14%] text-right' },
+  { name: 'Faixa Roxa', color1: '#9B00DC', color2: '#000', alignment: 'pr-[13%] text-right' },
+  { name: 'Faixa Marrom', color1: '#603911', color2: '#000', alignment: 'pr-[7%] text-right' },
+  { name: 'Faixa Preta', color1: '#010101', color2: '#FF0001', alignment: 'pr-[13%] text-right' },
+];
 
 export default function Home() {
   const [name, setName] = useState('Matheus A. Quinquinato');
@@ -33,6 +52,13 @@ export default function Home() {
     if (selectedBelt) {
       setBelt(selectedBelt);
     }
+  }
+
+  interface BeltsProps {
+    name: string;
+    color1: string;
+    color2: string;
+    alignment: string;
   }
 
   const contentRef = useRef<HTMLDivElement | null>(null);
